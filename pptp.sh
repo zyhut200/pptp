3fa553183af46eae7fb7a4b26345bc3d9dabe4b6
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2022-12-08 18:52:38 +0800

    Update pptp.sh

--- pptp.sh
+++ pptp.sh
@@ -1,3 +1,5 @@
+yum update -y
+yum install curl -y
 yum -y install wget 
 wget https://raw.githubusercontent.com/andanyoung/centos7-pptp/master/CentOS7-pptp-host1plus.sh
 chmod +x ./CentOS7-pptp-host1plus.sh
